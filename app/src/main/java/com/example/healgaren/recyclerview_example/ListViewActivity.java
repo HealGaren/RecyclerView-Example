defc05f799e2f03fb4b7dd7d9ad49f315e606292
HealGaren <healgaren99@gmail.com>
2018-08-25 20:34:09 +0900

    리스트뷰가 TodoVo를 받아서 토대로 띄우도록 하고, 목업 데이터 추가

--- app/src/main/java/com/example/healgaren/recyclerview_example/ListViewActivity.java
+++ app/src/main/java/com/example/healgaren/recyclerview_example/ListViewActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.Arrays;
+
 public class ListViewActivity extends AppCompatActivity {
 
     ListView listView;
@@ -19,6 +21,18 @@ public class ListViewActivity extends AppCompatActivity {
 
         adapter = new ListViewAdapter();
         listView.setAdapter(adapter);
+
+        adapter.addAllTodo(Arrays.asList(
+                new TodoVo("a", "안녕하세요"),
+                new TodoVo("asdf", "안asd"),
+                new TodoVo("zz", "안녕sadf"),
+                new TodoVo("dd", "안zz세요"),
+                new TodoVo("ff", "안녕z세요")
+        ));
+
+        for (int i = 0; i < 100; i++) {
+            adapter.addTodo(new TodoVo("title" + i, "content" + i));
+        }
 
     }
 }
--- app/src/main/java/com/example/healgaren/recyclerview_example/ListViewAdapter.java
+++ app/src/main/java/com/example/healgaren/recyclerview_example/ListViewAdapter.java
@@ -4,12 +4,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListViewAdapter extends BaseAdapter {
 
+    List<TodoVo> todoList = new ArrayList<>();
+
+
+    public void addTodo(TodoVo todo) {
+        todoList.add(todo);
+    }
+
+    public void addAllTodo(List<TodoVo> todoList) {
+        this.todoList.addAll(todoList);
+    }
+
     @Override
     public int getCount() {
-        return 5;
+        return todoList.size();
     }
 
     @Override
@@ -26,6 +41,15 @@ public class ListViewAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.item_todo, parent, false);
+
+
+        TextView textTitle = view.findViewById(R.id.textTodoTitle);
+        TextView textContent = view.findViewById(R.id.textTodoContent);
+
+        TodoVo todo = todoList.get(position);
+
+        textTitle.setText(todo.getTitle());
+        textContent.setText(todo.getContent());
 
         return view;
     }
