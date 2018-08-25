85794b491fe7e7ca822fecb934c7e14370ced2a5
HealGaren <healgaren99@gmail.com>
2018-08-25 21:18:33 +0900

    리사이클러뷰 어댑터의 각 메서드에 로그 추가

--- app/src/main/java/com/example/healgaren/recyclerview_example/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/healgaren/recyclerview_example/RecyclerViewAdapter.java
@@ -2,6 +2,7 @@ package com.example.healgaren.recyclerview_example;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,6 +32,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         View view = inflater.inflate(R.layout.item_todo, parent, false);
 
+        Log.d("YEC", "onCreateViewHolder 실행");
+
         return new ViewHolder(view);
     }
 
@@ -40,6 +43,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         holder.textTitle.setText(todo.getTitle());
         holder.textContent.setText(todo.getContent());
+
+        Log.d("YEC", position + "번째 아이템의 onBindViewHolder 실행");
     }
 
     @Override
@@ -56,6 +61,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             super(itemView);
             textTitle = itemView.findViewById(R.id.textTodoTitle);
             textContent = itemView.findViewById(R.id.textTodoContent);
+            Log.d("YEC", "findViewById 실행");
         }
 
     }
